2a8a91adf837b77fe485d53f5dfd3bdda4fc226a
Daniel Han <hex0cter@gmail.com>
2020-01-06 19:27:28 +0200

    Add shortcut for Respberry Pi

---
+++ bin/start-play.sh
@@ -0,0 +1,8 @@
+# ~/.config/autostart/youtube.desktop
+# [Desktop Entry]
+# Type=Application
+# Exec=/home/pi/start-play.sh
+
+export DISPLAY=:0
+sh -c 'chromium-browser --incognito --app=https://solna.xyz?uid=1Vpsm4Yy --kiosk' &
+cec-client | $HOME/cec-remote.sh
